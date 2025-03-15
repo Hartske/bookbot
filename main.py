6fe47bebd6104f9b660bd7fcdeb57e693dd26a2c
hartske <keatoru@gmail.com>
2025-03-15 13:10:15 -0700

    added sys.argv function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     get_num_words,
     get_num_characters,
@@ -7,7 +9,7 @@ from stats import (
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_characters = get_num_characters(text)
@@ -37,6 +39,7 @@ def print_report(file_path, word_count, chars_list):
         print(f"{char}: {count}")
     print("============= END ===============")
 
-
-
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 main()
