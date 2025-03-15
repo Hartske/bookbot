218cc890af2ba0d7b16ba88534f2f80e9b408c45
hartske <keatoru@gmail.com>
2025-03-15 12:29:08 -0700

    built print_report

--- main.py
+++ main.py
@@ -16,12 +16,25 @@ def main():
             char_list.append({"char": char, "count": count})
     char_list.sort(reverse=True, key=sort_num_characters)
 
-    print(f"{num_words} words found in the document")
-    print(char_list)
+    print_report(book_path, num_words, char_list)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def print_report(file_path, word_count, chars_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    # Counter loop
+    for char_dict in chars_list:
+        char = char_dict["char"]
+        count = char_dict["count"]
+        print(f"{char}: {count}")
+    print("============= END ===============")
+
 
 
 main()
